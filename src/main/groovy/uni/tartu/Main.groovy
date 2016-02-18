28fd21f3ae4306a5ead8893bb848ec510ca70ff2
Levani Kokhreidze <levani92@ut.ee>
2016-02-18 23:30:35 +0200

    refactoring

--- src/main/groovy/uni/tartu/Main.groovy
+++ src/main/groovy/uni/tartu/Main.groovy
@@ -11,8 +11,9 @@ import static uni.tartu.parser.Parser.parse
  **/
 
 class Main {
-    public static void main(String[] args) {
-        URL csv = Main.class.getClassLoader().getResource("test-data-1.csv")
-        Algorithm.create(parse(new File(csv.toURI())))
-    }
+	public static void main(String[] args) {
+		URL csv = Main.class.getClassLoader().getResource("test-data-1.csv")
+		Algorithm algorithm = new Algorithm(parse(new File(csv.toURI())))
+		algorithm.traverse()
+	}
 }
