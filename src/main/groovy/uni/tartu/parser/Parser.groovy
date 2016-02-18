3b5af6a89c8b686c9f8aeb2cbe216fe4ccec9675
Levani Kokhreidze <levani92@ut.ee>
2016-02-18 23:43:34 +0200

    @CompileStatic added

--- src/main/groovy/uni/tartu/parser/Parser.groovy
+++ src/main/groovy/uni/tartu/parser/Parser.groovy
@@ -1,24 +1,25 @@
 package uni.tartu.parser
 
+import groovy.transform.CompileStatic
+
 /**
  * author: lkokhreidze
  * date: 2/18/16
  * time: 8:59 PM
  **/
 
+@CompileStatic
 class Parser {
 	public static def parse(File file) {
 		def lines = file.readLines()
 		def keys = lines[0].split(',').collect { trim(it) }
 		lines[1..-1].collect { line ->
 			def i = 0, values = line.split(',', -1)
-			keys.inject([:]) { map, key ->
-				map << [(key): trim(values[i++])]
-			}
+			keys.inject([:]) { m, k -> m << [(k): trim(values[i++])] }
 		}
 	}
 
 	private static def trim(String str) {
-		str.replace("\"", '')
+		str.replace('\"', '')
 	}
 }
