4afa8f481c7e1d1e40f8c3b4ed152f28b83ebdc8
Levani Kokhreidze <levani92@ut.ee>
2016-02-18 19:47:48 +0200

    initial commit

---
+++ src/main/groovy/uni/tartu/Main.groovy
@@ -0,0 +1,16 @@
+package uni.tartu
+
+import uni.tartu.discovery.Algorithm
+
+/**
+ * author: lkokhreidze
+ * date: 2/18/16
+ * time: 7:33 PM
+ **/
+
+class Main {
+    public static void main(String[] args) {
+        new Algorithm()
+                .create()
+    }
+}
---
+++ src/main/groovy/uni/tartu/discovery/Algorithm.groovy
@@ -0,0 +1,13 @@
+package uni.tartu.discovery
+
+/**
+ * author: lkokhreidze
+ * date: 2/18/16
+ * time: 7:38 PM
+ **/
+
+class Algorithm {
+    public void create() {
+        printf "discover me!"
+    }
+}
