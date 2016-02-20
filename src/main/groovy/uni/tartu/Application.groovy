e470e320f2cd46fca1353cd07be766063cd4205a
Levani Kokhreidze <levani92@ut.ee>
2016-02-20 21:12:15 +0200

    spring-jetty initial configuration

---
+++ src/main/groovy/uni/tartu/Application.groovy
@@ -0,0 +1,17 @@
+package uni.tartu
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+/**
+ * author: lkokhreidze
+ * date: 2/20/16
+ * time: 6:57 PM
+ **/
+
+@SpringBootApplication
+class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,16 +1,9 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: lkokhreidze
-  Date: 2/18/16
-  Time: 6:58 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
     <title></title>
   </head>
   <body>
-
+  <h1>Initial Configuration</h1>
   </body>
 </html>
